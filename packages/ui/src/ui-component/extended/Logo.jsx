1f999d58cb3fc2fd42a498ca631061188fcab713
unknown <akmalfirmansyah26@gmail.com>
2024-12-02 18:26:38 +0700

    Initial commit

---
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -0,0 +1,22 @@
+import logo from '@/assets/images/paham_seo_logo.png'
+import logoDark from '@/assets/images/paham_seo_logo_dark.png'
+
+import { useSelector } from 'react-redux'
+
+// ==============================|| LOGO ||============================== //
+
+const Logo = () => {
+    const customization = useSelector((state) => state.customization)
+
+    return (
+        <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row' }}>
+            <img
+                style={{ objectFit: 'contain', height: 'auto', width: 150 }}
+                src={customization.isDarkMode ? logoDark : logo}
+                alt='Flowise'
+            />
+        </div>
+    )
+}
+
+export default Logo
